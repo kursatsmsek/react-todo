c96f2346d2716f086b152a4f07cca1ed34305c23
Kürşat Şimşek <kursatsimsek@Kursats-MacBook-Pro.local>
2021-10-03 20:41:20 +0300

    the clear completed button completed

--- src/components/sections/footer.js
+++ src/components/sections/footer.js
@@ -1,32 +1,35 @@
 import React from 'react';
 
 function Footer({ leftTodos, todos, setTodos }) {
-    return (
-        <div>
-            <footer className="footer">
-              <span className="todo-count">
-                <strong>{leftTodos} </strong>
-                  items left
-              </span>
+  const clearCompleted = () => {
+    setTodos(todos.filter(todo => todo.completed !== true));
+  }
+  return (
+    <div>
+        <footer className="footer">
+          <span className="todo-count">
+            <strong>{leftTodos} </strong>
+              items left
+          </span>
 
-              <ul className="filters">
-                <li>
-                  <a className="selected">All</a>
-                </li>
-                <li>
-                  <a>Active</a>
-                </li>
-                <li>
-                  <a>Completed</a>
-                </li>
-              </ul>
+          <ul className="filters">
+            <li>
+              <a className="selected">All</a>
+            </li>
+            <li>
+              <a>Active</a>
+            </li>
+            <li>
+              <a>Completed</a>
+            </li>
+          </ul>
 
-              <button className="clear-completed">
-                Clear completed
-              </button>
-            </footer>
-        </div>
-    )
+          <button className="clear-completed" onClick={clearCompleted}>
+            Clear completed
+          </button>
+        </footer>
+    </div>
+)
 }
 
 export default Footer;
